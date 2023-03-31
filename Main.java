af5144d520c05060a31dbbad7487f61ee3332309
nat_soloviova <natsolovyova2@gmail.com>
2023-03-31 11:23:00 +0300

    add dz-5

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int aSumB = a + b;
+        int cSumD = c + d;
+        boolean result = aSumB > cSumD ? true : false;
+        System.out.println(result);
+        System.out.println(++aSumB > cSumD - 2);
+        System.out.println(aSumB % 2 == 0 || cSumD % 2 == 0);
+    }
+}
