4b5c5577936afa8f4f4f39ba2d3fc46fb4992bd8
nat_soloviova <natsolovyova2@gmail.com>
2023-04-01 23:49:45 +0300

    add updates to Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,8 @@ public class Main {
         int d = 4;
         int aSumB = a + b;
         int cSumD = c + d;
-        boolean result = aSumB > cSumD ? true : false;
+        boolean result = aSumB > cSumD;
         System.out.println(result);
-        System.out.println(++aSumB > cSumD - 2);
-        System.out.println(aSumB % 2 == 0 || cSumD % 2 == 0);
+        System.out.println(++aSumB > (cSumD-=2));
     }
 }
